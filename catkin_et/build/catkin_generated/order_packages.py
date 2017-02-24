48f1e71720239638501692977435fc6a8b54b165
mavro10600 <maurofimec@gmail.com>
2017-02-24 14:56:21 -0600

    creacion de un workspace

---
+++ catkin_et/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/mavro/Finder/catkin_et/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/mavro/Finder/catkin_et/devel;/home/mavro/catkin_new/devel;/home/mavro/bebop_ws/devel;/opt/ros/indigo".split(';') if "/home/mavro/Finder/catkin_et/devel;/home/mavro/catkin_new/devel;/home/mavro/bebop_ws/devel;/opt/ros/indigo" != "" else []

9c23a24518d408d1b8d8a7fd55205046f21d59b0
mavro10600 <maurofimec@gmail.com>
2017-04-07 14:39:26 -0500

    modificque para que el control no haga cosas raras

--- catkin_et/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mavro/Finder/Finder/catkin_et/src
-# Build directory: /home/mavro/Finder/Finder/catkin_et/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
-SUBDIRS(beginner_tutorials)
-SUBDIRS(teleoperation)

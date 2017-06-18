06bbaaa9cb028abb07cd14c7ba14932f9468f681
protonauta <pinedacesar94@hotmail.com>
2017-06-17 21:22:32 -0500

    se agregó restart de dynamixel y lectura de carga

--- catkin_et/src/my_dynamixel_tutorial/src/dynamixel_publisher.cpp
+++ catkin_et/src/my_dynamixel_tutorial/src/dynamixel_publisher.cpp
@@ -1,10 +1,14 @@
 #include <ros/ros.h>
 #include <sensor_msgs/Joy.h>
 #include <std_msgs/Float64.h>
-#define Abutton joy->buttons[0]
-#define Bbutton joy->buttons[1]
+#include <dynamixel_msgs/JointState.h>
+#include <dynamixel_controllers/RestartController.h>
+#define 	Abutton 	joy->buttons[0]
+#define 	Bbutton		joy->buttons[1]
+#define 	startButton	joy->buttons[7] 
 
 std_msgs::Float64 gripper_angle;
+bool restart = false;
 
 void joyCallback(const sensor_msgs::Joy::ConstPtr& joy){
 	if( Abutton == 1.0){
@@ -17,24 +21,45 @@ void joyCallback(const sensor_msgs::Joy::ConstPtr& joy){
 			gripper_angle.data -= 0.05;
 		}
 	}
+	if(startButton == 1.0){//prueba
+		//restart dynamixel if error
+		restart= true;
+	}
 
 }
+void dynamixelCallback(const dynamixel_msgs::JointState::ConstPtr& dynamixel_status){
+	std::cout<< float(dynamixel_status->load) <<std::endl;
+	float load = 1;
+}
+
 int main(int argc, char **argv){
 	std::cout << "starting publishing gripper angles"<<std::endl;
  	ros::init(argc, argv, "dynamixel_node");
   	ros::NodeHandle n;
-  	ros::Publisher gripper_pub = n.advertise<std_msgs::Float64>("/tilt_controller/command", 1000);
-	ros::Subscriber subJoy 	= n.subscribe<sensor_msgs::Joy>("joy",10,joyCallback); 
-	ros::Rate loop_rate(10);
+	//ros::ServiceClient dynamixel_client = n.serviceClient<dynamixel_controller::RestartController>("/dynamixel_manager/ttyUSB0/start_controller");
+	ros::ServiceClient dynamixel_client = n.serviceClient<dynamixel_controllers::RestartController>("/restart_controller/ttyUSB0");
 	
+	dynamixel_controllers::RestartController dynamixel_srv;
+  	dynamixel_srv.request.port_name = "ttyUSB0";
+	//dynamixel_srv.request. 
+
+  	ros::Publisher gripper_position_pub = n.advertise<std_msgs::Float64>("/tilt_controller/command", 1000);
+
+	ros::Subscriber joy_sub	= n.subscribe<sensor_msgs::Joy>("joy",10,joyCallback),
+					dynamixel_status_sub = n.subscribe<dynamixel_msgs::JointState>("/tilt_controller/state",10,dynamixelCallback);
+	
+	ros::Rate loop_rate(10);
 
 	gripper_angle.data = 0.0;
 
   	while (ros::ok()){
-    gripper_pub.publish(gripper_angle);
-    ros::spinOnce();
-
-    loop_rate.sleep();
+    	gripper_position_pub.publish(gripper_angle);
+		if(restart == true){
+			dynamixel_client.call(dynamixel_srv);
+			restart=false;
+		}
+    	ros::spinOnce();
+    	loop_rate.sleep();
 	}
 	return 0;
 }

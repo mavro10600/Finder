84f9c715dff8f2358e8188e81524b9a696736091
Gerardo Ramos <ge_ra_va@hotmail.com>
2017-06-29 23:57:55 -0500

    publicadores en interface y mejora de launch

--- setupFinder.sh
+++ setupFinder.sh
@@ -2,5 +2,5 @@
 echo "Instalación de paquetes para el funcionamiento del FinDER v3"
 echo ""
 
-sudo apt-get install ros-indigo-joy libzbar-dev ros-indigo-usb-cam ros-indigo-dynamixel-motor ros-indigo-web-video-server ros-indigo-roswww
+sudo apt-get install ros-indigo-joy libzbar-dev ros-indigo-usb-cam ros-indigo-dynamixel-motor ros-indigo-web-video-server ros-indigo-roswww ros-indigo-hokuyo-node
 

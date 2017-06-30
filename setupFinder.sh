50fea5fe5eb289c0bce2f9bc088b5a657d47e76d
Gerardo Ramos <ge_ra_va@hotmail.com>
2017-06-29 20:24:55 -0500

    Script para instalar paquetes

---
+++ setupFinder.sh
@@ -0,0 +1,6 @@
+#!/bin/bash  
+echo "Instalación de paquetes para el funcionamiento del FinDER v3"
+echo ""
+
+sudo apt-get install ros-indigo-joy libzbar-dev ros-indigo-usb-cam ros-indigo-dynamixel-motor ros-indigo-web-video-server ros-indigo-roswww
+
